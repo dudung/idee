bb461edc3ea9b510e48848bdca7b109de05d6a4f
Sparisoma Viridi <dudung@gmail.com>
2022-05-02 06:19:56 +0700

    Update 02.h

--- src/2022/05/02.h
+++ src/2022/05/02.h
@@ -3,7 +3,13 @@
   Idea hit today
 */
 
+#include<vector>
+
 #ifndef TAG_2022_05_02
 #define TAG_2022_05_02
-note = ""
+
+void content() {
+  vector<string> lines;
+  lines.push_back("");
+}
 #endif
