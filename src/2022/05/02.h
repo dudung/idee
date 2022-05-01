1b428fae31b85dc0648a39dff9ae506bc52ecef3
Sparisoma Viridi <dudung@gmail.com>
2022-05-02 06:47:41 +0700

    Update 02.h

--- src/2022/05/02.h
+++ src/2022/05/02.h
@@ -12,19 +12,19 @@
 #define TAG_2022_05_02
 
 vector<string> content(void) {
-  // compound of paragraphs
+  // irregular four lines poem
   vector<string> lines
 
-  // 1st paragraph
-  string p = "
-  Jalan terblok mobil di Kolam
-  Menjaga jalan tetap lapang
-  Ke Pusdai ternyata Gasibu
-  Kecuali hujan boleh di sana
+  // 1st stanza
+  string p = "\
+  Jalan terblok mobil di Kolam\n\
+  Menjaga jalan tetap lapang\n\
+  Ke Pusdai ternyata Gasibu\n\
+  Kecuali hujan boleh di sana\
   ";
   lines.push_back(p);
 
-  // nth paragraph
+  // nth stanza
   string p = "
   ";
   lines.push_back(p);
